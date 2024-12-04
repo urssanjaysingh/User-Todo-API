a7f28713c027c80abd3d23eedc3cb1c1dc03c1a2
urssanjaysingh <sanjaysingh26112000@gmail.com>
2024-12-04 15:52:20 +0530

    Ready to Deploy

--- server.js
+++ server.js
@@ -11,43 +11,15 @@ connectDB();
 
 const app = express();
 
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/welcome.html");
+});
+
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
-
-app.get("/", (req, res) => {
-    res.send(`
-        Welcome to the User Todo API! Here are the available routes:
-
-        **User Routes:**
-        - POST /users/register: Register a new user
-        - POST /users/login: Log in a user
-        - DELETE /users/logout: Log out a user
-        - POST /users/forgot-password: Request a password reset
-        - GET /users/profile: Fetch user profile
-        - PATCH /users/update-profile: Update user profile
-        - PATCH /users/update-password: Update user password
-        - DELETE /users/delete-profile: Delete user profile
-        - PATCH /users/remove-profile-picture: Remove profile picture
-        - PATCH /users/update-profile-picture: Update profile picture
-        - GET /users/all: Fetch all users (admin only)
-        - GET /users/:id: Fetch a single user (admin only)
-        - PATCH /users/:id: Update user role (admin only)
-        - DELETE /users/:id: Delete a user (admin only)
-
-        **Todo Routes:**
-        - POST /todos/add: Add a new todo
-        - GET /todos/user-todos: Fetch all todos of the authenticated user
-        - PATCH /todos/update-todo-status/:id: Update the status of a todo
-        - GET /todos/filter-todo: Get todos by status (admin only)
-        - GET /todos/all: Fetch all todos (admin only)
-        - GET /todos/:id: Fetch a single todo
-        - PATCH /todos/:id: Update a todo
-        - DELETE /todos/:id: Delete a todo
-
-        Explore these routes to manage users and todos effectively.
-    `);
-});
 
 app.use("/users", userRoutes);
 app.use("/todos", todoRoutes);
